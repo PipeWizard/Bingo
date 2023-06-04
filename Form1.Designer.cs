46c5b07c5d1269575086c92e45703f3dcfbf75a5
CM <codyjgm@gmail.com>
2023-06-03 22:09:56 -0400

    Initial Commit of all files

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Bingo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonProgress = new System.Windows.Forms.Button();
+            this.buttonBoard = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonGetBingos = new System.Windows.Forms.Button();
+            this.buttonLogbingo = new System.Windows.Forms.Button();
+            this.bingoBox = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonProgress
+            // 
+            this.buttonProgress.Location = new System.Drawing.Point(3, 3);
+            this.buttonProgress.Name = "buttonProgress";
+            this.buttonProgress.Size = new System.Drawing.Size(155, 23);
+            this.buttonProgress.TabIndex = 0;
+            this.buttonProgress.Text = "Clear Progress";
+            this.buttonProgress.UseVisualStyleBackColor = true;
+            // 
+            // buttonBoard
+            // 
+            this.buttonBoard.Location = new System.Drawing.Point(3, 32);
+            this.buttonBoard.Name = "buttonBoard";
+            this.buttonBoard.Size = new System.Drawing.Size(155, 23);
+            this.buttonBoard.TabIndex = 1;
+            this.buttonBoard.Text = "New Board";
+            this.buttonBoard.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonGetBingos);
+            this.panel1.Controls.Add(this.buttonLogbingo);
+            this.panel1.Controls.Add(this.buttonProgress);
+            this.panel1.Controls.Add(this.buttonBoard);
+            this.panel1.Location = new System.Drawing.Point(627, 300);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(161, 116);
+            this.panel1.TabIndex = 2;
+            // 
+            // buttonGetBingos
+            // 
+            this.buttonGetBingos.Location = new System.Drawing.Point(3, 90);
+            this.buttonGetBingos.Name = "buttonGetBingos";
+            this.buttonGetBingos.Size = new System.Drawing.Size(154, 23);
+            this.buttonGetBingos.TabIndex = 3;
+            this.buttonGetBingos.Text = "Get Bingo Log";
+            this.buttonGetBingos.UseVisualStyleBackColor = true;
+            // 
+            // buttonLogbingo
+            // 
+            this.buttonLogbingo.Location = new System.Drawing.Point(3, 61);
+            this.buttonLogbingo.Name = "buttonLogbingo";
+            this.buttonLogbingo.Size = new System.Drawing.Size(154, 23);
+            this.buttonLogbingo.TabIndex = 2;
+            this.buttonLogbingo.Text = "Log Bingo";
+            this.buttonLogbingo.UseVisualStyleBackColor = true;
+            // 
+            // bingoBox
+            // 
+            this.bingoBox.Location = new System.Drawing.Point(627, 272);
+            this.bingoBox.Multiline = false;
+            this.bingoBox.Name = "bingoBox";
+            this.bingoBox.Size = new System.Drawing.Size(157, 22);
+            this.bingoBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 445);
+            this.Controls.Add(this.bingoBox);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Bingo";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonProgress;
+        private System.Windows.Forms.Button buttonBoard;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonLogbingo;
+        private System.Windows.Forms.Button buttonGetBingos;
+        private System.Windows.Forms.TextBox bingoBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,395 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Bingo
+{
+    public partial class Form1 : Form
+    {
+        private DataGridView dGV = new DataGridView();
+
+        //colours
+        private readonly Color cellBackColor = Color.White;
+        private readonly Color cellGridColor = Color.Black;
+        private readonly Color cellMarked = Color.LightSalmon;
+        private Random random = new Random();
+
+        // TODO - maybe turn all for loops into a series of 'GetGridData()' methods
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            Load += new EventHandler(Form1_Load);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetupLayout();
+            SetupDataGridView();
+            PopulateDataGridView();
+        }
+
+        private void SetupLayout()
+        {
+            bingoBox.Visible = false;
+            Menu = new MainMenu();
+
+            MenuItem item = new MenuItem("File");
+                Menu.MenuItems.Add(item);
+                item.MenuItems.Add("Save", new EventHandler(Save_Click));
+                item.MenuItems.Add("Load", new EventHandler(Load_Click));
+
+            item = new MenuItem("Edit");
+                Menu.MenuItems.Add(item);
+                item.MenuItems.Add("Clear Board", new EventHandler(Clear_Board));
+                item.MenuItems.Add("Clear Progress", new EventHandler(Clear_Progress));
+                item.MenuItems.Add("New Board", new EventHandler(New_Board));
+
+            buttonProgress.MouseClick += Button_Clear_Progress;
+            buttonBoard.MouseClick += Button_New_Board;
+            buttonLogbingo.MouseClick += Button_Log_Bingo;
+            buttonGetBingos.MouseClick += Button_Get_Bingos;
+        }
+
+        private async void Button_Get_Bingos(object sender, MouseEventArgs e)
+        {
+            string currentDir = AppDomain.CurrentDomain.BaseDirectory;
+            string file_Bingos = Path.Combine(currentDir, @"..\..\saves\bingos");
+            string fPath_Bingos = Path.GetFullPath(file_Bingos);
+
+            using (FileStream fs = File.Open(fPath_Bingos, FileMode.Open))
+            {
+                //BinaryFormatter bf = new BinaryFormatter();
+                using (BinaryReader br = new BinaryReader(fs))
+                {
+                    bingoBox.Text = "Total number of Bingos: " + br.ReadInt32();
+                    bingoBox.Visible = true;
+                    bingoBox.ReadOnly = true;
+                }
+            }
+            await Task.Delay(5000);
+            bingoBox.Visible = false;
+        }
+
+        private void Button_Log_Bingo(object sender, MouseEventArgs e)
+        {
+            string currentDir = AppDomain.CurrentDomain.BaseDirectory;
+            string file_Bingos = Path.Combine(currentDir, @"..\..\saves\bingos");
+            string fPath_Bingos = Path.GetFullPath(file_Bingos);
+            
+            int acc = 0;
+            using (var stream = File.Open(fPath_Bingos, FileMode.OpenOrCreate))
+            {
+                using (BinaryReader br = new BinaryReader(stream, Encoding.UTF8, true))
+                {
+                    //if (br.PeekChar() != -1)
+                    //{
+                    acc = br.ReadInt32();
+                    //}
+                }
+                using (BinaryWriter bw = new BinaryWriter(stream, Encoding.UTF8, false))
+                {
+                    Console.WriteLine(acc);
+                    acc++;
+                    bw.Seek(0, SeekOrigin.Begin);
+                    bw.Write(acc);
+                    Console.WriteLine(acc);
+                }
+            }
+        }
+
+        private void New_Board(object sender, EventArgs e)
+        {
+            Clear_Board(sender, e);
+            PopulateDataGridView();
+            RemoveEmptyRows();
+            dGV.ClearSelection();
+        }
+
+        private void Clear_Progress(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dGV.RowCount; i++)
+            {
+                for (int j = 0; j < dGV.ColumnCount; j++)
+                {
+                    dGV.Rows[i].Cells[j].Style.BackColor = cellBackColor;
+                }
+            }
+        }
+
+        private void Clear_Board(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dGV.RowCount; i++)
+            {
+                for (int j = 0; j < dGV.ColumnCount; j++)
+                {
+                    dGV.Rows[i].Cells[j].Value = null;
+                    dGV.Rows[i].Cells[j].Style.BackColor = cellBackColor;
+                }
+            }
+
+            dGV.ClearSelection();
+        }
+
+        private void Load_Click(object sender, EventArgs e)
+        {
+            string currentDir = AppDomain.CurrentDomain.BaseDirectory;
+            string file_Card = Path.Combine(currentDir, @"..\..\saves\card");
+            string fPath_Card = Path.GetFullPath(file_Card);
+            string file_Progress = Path.Combine(currentDir, @"..\..\saves\progress");
+            string fPath_Progress = Path.GetFullPath(file_Progress);
+
+            Clear_Board(sender, e);
+
+            if (File.Exists(fPath_Card))
+            {
+                using (var stream = File.Open(fPath_Card, FileMode.Open))
+                {
+                    using (BinaryReader reader = new BinaryReader(stream, Encoding.UTF8, false))
+                    {
+                        for (int i = 0; i < dGV.RowCount; i++)
+                        {
+                            for (int j = 0; j < dGV.ColumnCount; j++)
+                            {
+                                dGV.Rows[i].Cells[j].Value = reader.ReadString();
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (File.Exists(fPath_Progress))
+            {
+                using (var stream = File.Open(fPath_Progress, FileMode.Open))
+                {
+                    using (BinaryReader reader = new BinaryReader(stream, Encoding.UTF8, false))
+                    {
+                        //reader.BaseStream.Seek(0, SeekOrigin.Begin);
+                        for (int i = 0; i < dGV.RowCount; i++)
+                        {
+                            for (int j = 0; j < dGV.ColumnCount; j++)
+                            {
+                                if (reader.ReadBoolean())
+                                    dGV.Rows[i].Cells[j].Style.BackColor = cellMarked;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            string currentDir = AppDomain.CurrentDomain.BaseDirectory;
+            string file_Card = Path.Combine(currentDir, @"..\..\saves\card");
+            string fPath_Card = Path.GetFullPath(file_Card);
+            string file_Progress = Path.Combine(currentDir, @"..\..\saves\progress");
+            string fPath_Progress = Path.GetFullPath(file_Progress);
+
+            using (var stream = File.Open(fPath_Card, FileMode.Create))
+            {
+                using (BinaryWriter writer = new BinaryWriter(stream, Encoding.UTF8, false))
+                {
+                    for (int i = 0; i < dGV.RowCount; i++)
+                    {
+                        for (int j = 0; j < dGV.ColumnCount; j++)
+                        {
+                            writer.Write(dGV.Rows[i].Cells[j].Value.ToString());
+                        }
+                    }
+                }
+            }
+
+            using (var stream = File.Open(fPath_Progress, FileMode.Create))
+            {
+                using (BinaryWriter writer = new BinaryWriter(stream,Encoding.UTF8, false))
+                {
+                    for (int i = 0; i < dGV.RowCount; i++)
+                    {
+                        for (int j = 0; j < dGV.ColumnCount; j++)
+                        {
+                            if (dGV.Rows[i].Cells[j].Style.BackColor == cellMarked)
+                                writer.Write(true);
+                            else
+                                writer.Write(false);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void SetupDataGridView()
+        {
+            Controls.Add(dGV);
+            dGV.ColumnCount = 5;
+            dGV.ColumnHeadersDefaultCellStyle.BackColor = cellBackColor;
+            dGV.ColumnHeadersDefaultCellStyle.Font = new Font(dGV.Font, FontStyle.Bold);
+
+            dGV.Name = "bingoDataGridView";
+            //dataGridView.Location = new Point(8, 8);
+            dGV.Size = new Size(800, 800);  //scale off monitor resolution? /shrug
+            dGV.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dGV.CellBorderStyle = DataGridViewCellBorderStyle.Single;
+            dGV.GridColor = cellGridColor;
+            dGV.RowHeadersVisible = false;
+            //dataGridView.AutoResizeRows();
+            dGV.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dGV.AutoResizeColumns();
+            dGV.RowsDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dGV.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            dGV.AllowUserToAddRows = false;
+            dGV.AllowUserToResizeRows = false;
+            dGV.ReadOnly = true;
+            dGV.AllowUserToResizeColumns = false;
+            dGV.ColumnHeadersHeight = 40;
+            dGV.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+
+            foreach (DataGridViewColumn column in dGV.Columns)
+            {
+                column.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                column.SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+
+            //column defines
+            dGV.Columns[0].Name = "B";
+            dGV.Columns[1].Name = "I";
+            dGV.Columns[2].Name = "N";
+            dGV.Columns[3].Name = "G";
+            dGV.Columns[4].Name = "O";
+
+            ////row defines
+            //dataGridView.RowCount = 5;
+
+            dGV.SelectionMode = DataGridViewSelectionMode.CellSelect;
+            dGV.MultiSelect = false;
+            dGV.Dock = DockStyle.Fill;
+
+            // Event Registration
+            dGV.CellClick += new DataGridViewCellEventHandler(dGV_CellClick);
+        }
+
+        private void PopulateDataGridView()
+        {
+            string[] memes = { "Sulfuric acid is dead", "OpenTTD moment", "John disconnects something",
+                                "New Foundry still not done", "X acid is dead", "Train kill",
+                                ">5 refactor tags", ">15 refactor tags", "Incomprehensible screaming",
+                                "Why isn't research ticking?", "Server crashes", "Power problems",
+                                "Pipe says he'll do it later", "Making Pipe's stack a queue",
+                                "John forgets what he was doing", "Pipe takes forever joining",
+                                "Solder is dead", "Pipe complains about HE shells", "Fox lags out",
+                                "Pipe wages a useless war", "John forgets he's the host",
+                                "Fox assisted auto's", "Fox tries to offload work",
+                                "Fox complains about spaghetti", "Pipe mentions fluid capacity",
+                                "True Nukes almost crashes", "Update confusion", "John/Pipe spends ages in circuits",
+                                "It's just temporary", "Update changes recipes" };
+
+            RemoveEmptyRows();
+
+            // 2D array of 5x5 with an index randomizer that adds all the strings
+            // overrule the for loop for freespace
+            // for each index, random pick from list of options, ensure pick isn't in array
+            string[,] cards = new string[5, 5];
+            List<string> memeCopy = new List<string>();
+
+            for (int i = 0; i < memes.Length; i++)
+            {
+                string s = memes[i];
+                memeCopy.Add(s); // copies memes string[] to a memeCopy List<>
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    int indexed = random.Next(0, memeCopy.Count);
+                    string pick = memeCopy[indexed];
+                    memeCopy.RemoveAt(indexed);
+
+                    cards[i, j] = pick;
+                }
+            }
+
+            for (int r = 0; r < cards.GetLength(0); r++)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                row.CreateCells(dGV);
+
+                for (int c = 0; c < cards.GetLength(1); c++)
+                {
+                    row.Cells[c].Value = cards[r, c];
+                }
+                dGV.Rows.Insert(r, row);
+            }
+
+            dGV.ClearSelection();
+
+            foreach (DataGridViewRow row in dGV.Rows)
+            {
+                row.Height = 50;
+            }
+
+            memeCopy.Clear();
+            RemoveEmptyRows();
+        }
+
+        private void dGV_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                DataGridViewCell cell = dGV.Rows[e.RowIndex].Cells[e.ColumnIndex];
+
+                if (cell.Style.BackColor != cellMarked)
+                    cell.Style.BackColor = cellMarked;
+                else
+                    cell.Style.BackColor = cellBackColor;
+
+                dGV.ClearSelection();
+                //Console.WriteLine("Cell updated!");
+            }
+            catch (Exception ex)
+            {
+                if (ex is ArgumentOutOfRangeException) {} // This should be fixed with code, not try-catch
+                else
+                {
+                    Console.WriteLine("Contact Pipe with the error:");
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("...And with steps to recreate");
+                }
+            }
+        }
+
+        private void Button_Clear_Progress(object sender, MouseEventArgs e)
+        {
+            Clear_Progress(sender, e);
+        }
+
+        private void Button_New_Board(object sender, MouseEventArgs e)
+        {
+            New_Board(sender, e);
+        }
+
+        private void RemoveEmptyRows()
+        {
+            for (int i = 0; i < dGV.RowCount; i++)
+            {
+                if (dGV.Rows[i].Cells[0].Value is null)
+                {
+                    dGV.Rows.RemoveAt(i);
+                }
+            }
+            dGV.ClearSelection();
+        }
+    }
+}
